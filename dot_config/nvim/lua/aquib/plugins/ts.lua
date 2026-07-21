c1de0c20e8687f75c7a2d48ddec38ec5da62724b
Aquib Baig <aquibbaig97@gmail.com>
2026-07-21 22:33:52 +0530

    fix(nvim): migrate treesitter to main

--- dot_config/nvim/lua/aquib/plugins/ts.lua
+++ dot_config/nvim/lua/aquib/plugins/ts.lua
@@ -1,36 +1,79 @@
+local languages = {
+	"lua",
+	"python",
+	"javascript",
+	"typescript",
+	"tsx",
+	"go",
+	"rust",
+	"c",
+	"cpp",
+	"java",
+	"bash",
+	"json",
+	"html",
+	"css",
+	"nu",
+}
+
+local filetypes = {
+	"lua",
+	"python",
+	"javascript",
+	"javascriptreact",
+	"typescript",
+	"typescriptreact",
+	"go",
+	"rust",
+	"c",
+	"cpp",
+	"java",
+	"sh",
+	"json",
+	"html",
+	"css",
+	"nu",
+}
+
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
+	lazy = false,
 	build = ":TSUpdate",
-	event = { "BufReadPost", "BufNewFile" },
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"lua",
-				"python",
-				"javascript",
-				"typescript",
-				"tsx",
-				"go",
-				"rust",
-				"c",
-				"cpp",
-				"java",
-				"bash",
-				"json",
-				"html",
-				"css",
-				"nu",
-			},
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			indent = {
-				enable = true,
-			},
+	init = function()
+		local group = vim.api.nvim_create_augroup("aquib-treesitter", { clear = true })
+
+		vim.api.nvim_create_autocmd("FileType", {
+			group = group,
+			pattern = filetypes,
+			callback = function(args)
+				local ok = pcall(vim.treesitter.start, args.buf)
+				if ok then
+					vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end
+			end,
 		})
 	end,
-	dependencies = {
-		{ "nushell/tree-sitter-nu" },
-	},
+	config = function()
+		local treesitter = require("nvim-treesitter")
+
+		treesitter.setup({
+			install_dir = vim.fn.stdpath("data") .. "/site",
+		})
+
+		local installed = {}
+		for _, language in ipairs(treesitter.get_installed("parsers")) do
+			installed[language] = true
+		end
+
+		local missing = vim.tbl_filter(function(language)
+			return not installed[language]
+		end, languages)
+
+		if #missing > 0 and #vim.api.nvim_list_uis() > 0 then
+			vim.schedule(function()
+				treesitter.install(missing)
+			end)
+		end
+	end,
 }
